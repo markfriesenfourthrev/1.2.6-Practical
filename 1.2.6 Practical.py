5c14cd1ce8e27b9e9d1a12b459f3edf8213429e7
markfriesenfourthrev <99162366+markfriesenfourthrev@users.noreply.github.com>
2022-02-08 14:26:35 +1100

    Add files via upload

---
+++ 1.2.6 Practical.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# this is my first line of code
+
+print('My name is Mark')
+print('My favourite TV show is Money Heist')
+
+
+# In[ ]:
+
+
+# # Write a program to add, subtract, multiply and divide two numbers
+
